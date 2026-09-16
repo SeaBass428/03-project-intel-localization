350d6667b1e600a1083f2899f2b0b5b2bcd7a48a
Sebastian Orduno <114191724+SeaBass428@users.noreply.github.com>
2026-09-16 21:08:55 +0000

    Fix Bootstrap stylesheet link and add RTL language support script

---
+++ script.js
@@ -0,0 +1,24 @@
+// Languages that normally use a right-to-left writing direction.
+const rtlLanguageCodes = ["ar", "dv", "fa", "ha", "he", "ku", "ps", "sd", "ug", "ur", "yi"];
+
+// Read the first part of a language tag, such as "ar" from "ar-SA".
+function getBaseLanguage(language) {
+  return language.toLowerCase().split("-")[0].split("_")[0];
+}
+
+// Match the page direction to the language currently shown on the page.
+function updatePageDirection() {
+  const language = getBaseLanguage(document.documentElement.lang || "en");
+  const isRightToLeft = rtlLanguageCodes.includes(language);
+
+  document.documentElement.dir = isRightToLeft ? "rtl" : "ltr";
+}
+
+updatePageDirection();
+
+// Google Translate can change the html lang attribute after the page loads.
+const languageObserver = new MutationObserver(updatePageDirection);
+languageObserver.observe(document.documentElement, {
+  attributes: true,
+  attributeFilter: ["lang"]
+});
